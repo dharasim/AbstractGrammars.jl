85210ac8c779a391b81da94eaa81c6acea722acb
Daniel Harasim <daniel.harasim@epfl.ch>
2021-11-06 22:33:42 +0100

    delete old code

--- src/AtMosts.jl
+++ src/AtMosts.jl
@@ -28,18 +28,6 @@ getindex(xs::AtMost, i) = getfield(xs.vals, i)
 keys(xs::AtMost) = 1:length(xs)
 eltype(::Type{A}) where {T, A <: AtMost{T}} = T
 
-# function ==(xs::T1, ys::T2) where {T1 <: AtMost, T2 <: AtMost}
-#   xs.length == ys.length && all(isequal(x, y) for (x, y) in zip(xs, ys))
-# end
-
-# function hash(xs::AtMost, h::UInt)
-#   result = h
-#   for x in xs
-#     result = hash(x, result)
-#   end
-#   return result
-# end
-
 function iterate(xs::AtMost, i=0)
   if i == length(xs)
     nothing
@@ -60,42 +48,5 @@ end
 for N in 1:10
   @eval $(Symbol(:atmost, N))(xs...) = AtMost(xs..., limit=$N)
 end
-
-
-# function generate_code_atmost(n)
-#   struct_expr = quote
-#     struct $(Symbol(:AtMost, n)){T} <: AtMost{T}
-#       length::Length
-#     end
-#   end
-#   append!( # append field expressions
-#     struct_expr.args[2].args[3].args, 
-#     [:($(Symbol(:val, i)) :: T) for i in 1:n])
-  
-#   struct_args = struct_expr.args[2].args[3].args
-#   ex = quote
-#     function $(Symbol(:AtMost, n))(T::Type)
-#       new{T}(Length(0))
-#     end
-#   end
-#   push!(struct_args, ex.args[2])
-#   for k in 1:n
-#     vals = Symbol.(:val, 1:k)
-#     typed_vals = Expr.(:(::), vals, :T)
-#     ex = quote
-#       function $(Symbol(:AtMost, n))($(typed_vals...),) where T
-#         new{T}(Length($k), $(vals...))
-#       end
-#     end
-#     push!(struct_args, ex.args[2])
-#   end
-
-#   # ex = Expr(:block, struct_expr, constr_exprs)
-#   eval(struct_expr)
-# end
-
-# for n in 0:10
-#   generate_code_atmost(n)
-# end
 
 end # module
